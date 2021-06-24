d94c2224d8045111482085fe6dd01df38bf0a60c
Jones Kurashima <joneskurashima@gmail.com>
2021-06-24 19:20:47 -0300

    Aula 104 - Teclado Numérico

--- components/transaction_user.dart
+++ components/transaction_user.dart
@@ -24,30 +24,6 @@ class _State extends State<TransactionUser> {
       value: 211.30,
       date: DateTime.now(),
     ),
-    Transaction(
-      id: 't3',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
   ];
 
   _addTransaction(String title, double value) {
