78dce4c2c2e20da826a3ab1521b73fad7dc77cf4
Jones Kurashima <joneskurashima@gmail.com>
2021-08-23 19:57:30 -0300

    Aula 172 - Desafio Extraindo Widget

--- components/transaction_form.dart
+++ components/transaction_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'adaptative_button.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double, DateTime) onSubmit;
@@ -96,6 +97,10 @@ class _TransactionFormState extends State<TransactionForm> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
+                  AdaptativeButton(
+                    label: 'Nova Transação',
+                    onPressed: _submitForm(),
+                  ),
                   ElevatedButton(
                     child: Text('Nova Transação'),
                     //textColor: Colors.purple,
